4dc10efc074929ecb1235bc12685beeb9ebcf169
Yi Zhao <zhaoyi0113@gmail.com>
2018-03-09 08:12:12 +1100

    update style

--- app/components/CityWeather.jsx
+++ app/components/CityWeather.jsx
@@ -11,18 +11,20 @@ const City = ({city, deleteCity}) => {
       <td>{city.name}</td>
       <td>{city.main.temp}c</td>
 			<td>{forcast && `${forcast}c`}</td>
-			<td><Button className="pt-intent-primary" onClick={() => deleteCity(city.id)}>delete</Button></td>
+			<td><Button 
+						className="pt-icon-trash" onClick={() => deleteCity(city.id)}></Button></td>
     </tr>
   );
 };
 
 export default ({cities, deleteCity}) => {
   return (
-    <table style={{width: '100%'}}>
+    <table className="city-list" style={{width: '100%'}}>
       <tr>
         <th>City</th>
         <th>Today</th>
         <th>Forecast</th>
+        <th>Delete</th>
       </tr>
       {cities.map((city, i) => {
         return <City key={i} city={city} deleteCity={deleteCity} />;
